21456ee09bcc716ced43d4277eac6a20c22c0560
unknown <frodriguez-as@frodriguez-as.AVANTEK-SC.AVANTICA.AVANTICATEC.NET>
2014-07-18 10:46:51 -0600

    Rendering fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app =  express();
 
-app.set('views',  __dirname + 'server/views');
+app.set('views',  __dirname + '/server/views');
 app.set('view engine', 'jade');
 
 app.get('*', function(req, res){
